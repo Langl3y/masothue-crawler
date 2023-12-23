425a2b48358a55c23d85c355252cee32bdce838e
hieu <hieu.van@goatstudio.tech>
2023-12-24 03:17:50 +0700

    feat: time.sleep uncommented

--- data_processing/get_html.py
+++ data_processing/get_html.py
@@ -7,8 +7,8 @@ import random as rd
 def get_html(url):
     ua = UserAgent()
     headers = {"User-Agent": ua.random}
-    # sleep_period = rd.uniform(1, 3)
-    # time.sleep(sleep_period)
+    sleep_period = rd.uniform(1, 7)
+    time.sleep(sleep_period)
     response = requests.get(url, headers=headers)
     html_string = response.content.decode('utf-8')
 
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from city import *
 
 
 def main():
-    haiduong_crawler()
-    haiphong_crawler()
-    hatinh_crawler()
-    haugiang_crawler()
-    hoabinh_crawler()
+    # haiduong_crawler()
+    # haiphong_crawler()
+    # hatinh_crawler()
+    # haugiang_crawler()
+    # hoabinh_crawler()
     hochiminh_crawler()
     hungyen_crawler()
     khanhhoa_crawler()
